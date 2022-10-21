0cd6cb739306e9d8ceed1a9007f4e920807dcf19
Joseph Watkins <102884938+Jwatk13@users.noreply.github.com>
2022-10-21 16:14:23 -0400

    initialized components and constants

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-// import { useFonts } from 'expo-font';
+import { useFonts } from 'expo-font';
 
 import Home from './screens/Home';
 import Details from './screens/Details';
@@ -17,6 +17,16 @@ const theme = {
 }
 
 const App = () => {
+  const [loaded] = useFonts({
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+  });
+
+  if(!loaded) return null;
+
   return (
    <NavigationContainer theme={theme}>
       <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
